8cbbafc15c08d9a84bd62d03f55b66203e9c523e
Glen Vasa <glenvasa@gmail.com>
2020-11-24 11:34:16 -0500

    updated updateEvents function to filter events number based on user input/change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,77 @@
-import React, { Component } from 'react';
-import './App.css';
-import EventList from './EventList';
-import CitySearch from './CitySearch';
-import NumberofEvents from './NumberOfEvents';
-
+import React, { Component } from "react";
+import "./nprogress.css";
+import "./App.css";
+import EventList from "./EventList";
+import CitySearch from "./CitySearch";
+import NumberOfEvents from "./NumberOfEvents";
+import { extractLocations, getEvents } from "./api";
 
 class App extends Component {
-  render () {
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: 32,
+    currentLocation: "all",
+  };
+
+  updateEvents = (location, eventCount) => {
+    const { currentLocation, numberOfEvents } = this.state;
+    if (location) {
+      getEvents().then((events) => {
+        const locationEvents =
+          location === "all"
+            ? events
+            : events.filter((event) => event.location === location);
+        const filteredEvents = locationEvents.slice(0, numberOfEvents);
+        this.setState({
+          events: filteredEvents,
+          currentLocation: location,
+        });
+      });
+    } else {
+      getEvents().then((events) => {
+        const locationEvents =
+          currentLocation === "all"
+            ? events
+            : events.filter((event) => event.location === currentLocation);
+        const filteredEvents = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: filteredEvents,
+          numberOfEvents: eventCount,
+        });
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({
+          events: events,
+          locations: extractLocations(events),
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  render() {
     return (
-    <div className="App">
-     <CitySearch />
-     <EventList />
-     <NumberofEvents />
-    </div>
+      <div className="App">
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <NumberOfEvents
+          numberOfEvents={this.state.numberOfEvents}
+          updateEvents={this.updateEvents}
+        />
+        <EventList events={this.state.events} />
+      </div>
     );
   }
 }
