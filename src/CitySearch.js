3338a10bc30b787bb9943971b36fd3521784e5c9
Glen Vasa <glenvasa@gmail.com>
2020-11-30 08:59:40 -0500

    Added InfoAlert when user does not enter a city name on list

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { InfoAlert } from "./Alert";
 
 class CitySearch extends Component {
   state = {
@@ -12,16 +13,27 @@ class CitySearch extends Component {
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText:
+          "We can not find the city you are looking for. Please try another city",
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: "",
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
+      suggestions: [],
       showSuggestions: false,
+      infoText: "",
     });
 
     this.props.updateEvents(suggestion);
@@ -30,6 +42,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <h2 className="citysearch-instruction">Choose your closest city</h2>
         <input
           type="text"
