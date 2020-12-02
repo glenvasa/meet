e5ca9b3a5b272da7e0ed8a586020f99066a58345
Glen Vasa <glenvasa@gmail.com>
2020-12-02 18:27:22 -0500

    Added offline functionality

--- src/api.js
+++ src/api.js
@@ -26,6 +26,21 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  // if (result.data) {
+  //   var locations = extractLocations(result.data.events);
+  //   localStorage.setItem('lastEvents', JSON.stringify(result.data));
+  //   localStorage.setItem('locations', JSON.stringify(locations));
+  // }
+
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return {
+      events: JSON.parse(events).events,
+      locations: extractLocations(JSON.parse(events).events),
+    };
+  }
+
   const token = await getAccessToken();
 
   if (token) {
