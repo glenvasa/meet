d72a7a769e8519c9d6b414045ac0b3d4c9403ce1
Glen Vasa <glenvasa@gmail.com>
2020-12-05 11:13:19 -0500

    updated getEvents() function to retrieve lastEvents from localStorage when offline

--- src/api.js
+++ src/api.js
@@ -21,18 +21,18 @@ const checkToken = async (accessToken) => {
 export const getEvents = async () => {
   NProgress.start();
 
+  if (
+    !navigator.onLine &&
+    !window.location.href.startsWith("http://localhost")
+  ) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return JSON.parse(events).events;
+  }
+
   if (window.location.href.startsWith("http://localhost")) {
     NProgress.done();
     return mockData;
-  }
-
-  if (!navigator.onLine) {
-    const events = localStorage.getItem("lastEvents");
-    NProgress.done();
-    return {
-      events: JSON.parse(events).events,
-      locations: extractLocations(JSON.parse(events).events),
-    };
   }
 
   const token = await getAccessToken();
